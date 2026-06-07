4666232e327d29b0e23468c226f12a12ee8b2225
ethimobrasil <ethimnobrasil@gmail.com>
2026-06-07 04:18:12 -0300

    Primeiro Exercicio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,60 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado[2];
+  char codigoCarta[2][3];
+  char nomeCidade[2][20];
+  int populacao[2];
+  float pib[2];
+  int pontosTuristicos[2];
 
   // Área para entrada de dados
+  printf("Carta 1:\n");
+  
+  printf("Informe o Estado (Entre A e H): ");
+  scanf(" %c", &estado[0]);
+  
+  printf("Informe o Código da Carta: ");
+  scanf("%s", codigoCarta[0]);
+  
+  printf("Informe o Nome da Cidade: ");
+  scanf("%s",nomeCidade[0]);
+  
+  printf("Informe a População: ");
+  scanf("%d",&populacao[0]);
+  
+  printf("Informe o PIB: ");
+  scanf("%f", &pib[0]);
+
+  printf("Informe a quantidade de pontos turisticos: ");
+  scanf("%d", &pontosTuristicos[0]);
+
+  //Carta 2
+  printf("Carta 2:\n");
+  
+  printf("Informe o Estado (Entre A e H): ");
+  scanf(" %c", &estado[1]);
+  
+  printf("Informe o Código da Carta: ");
+  scanf("%s", codigoCarta[1]);
+  
+  printf("Informe o Nome da Cidade: ");
+  scanf("%s",nomeCidade[1]);
+  
+  printf("Informe a População: ");
+  scanf("%d",&populacao[1]);
+  
+  printf("Informe o PIB: ");
+  scanf("%f", &pib[1]);
+
+  printf("Informe a quantidade de pontos turisticos: ");
+  scanf("%d", &pontosTuristicos[1]);
 
   // Área para exibição dos dados da cidade
+  printf("Carta 1:\n Estado: %c\n  Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n PIB: %f\n Quantidades de pontos turísticos: %d\n.", estado[0], codigoCarta[0], nomeCidade[0], populacao[0], pib[0], pontosTuristicos[0]);
 
-return 0;
+  printf("Carta 1:\n Estado: %c\n  Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n PIB: %f\n Quantidades de pontos turísticos: %d\n.", estado[1], codigoCarta[1], nomeCidade[1], populacao[1], pib[1], pontosTuristicos[1]);
+
+
+  return 0;
 } 
