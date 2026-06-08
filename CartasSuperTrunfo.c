bc6b239a6b16ca93c5655f4a6b789f7a35d72927
ethimobrasil <ethimnobrasil@gmail.com>
2026-06-07 21:49:23 -0300

    Batalha de Cartas no Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,9 @@ int main() {
   float area[2];
   float pib[2];
   int pontosTuristicos[2];
+  float densidadePopulacional[2];
+  float pibPerCarpita[2];
+  float superPoder[2];
 
   // Área para entrada de dados
   printf("Carta 1:\n");
@@ -38,6 +41,10 @@ int main() {
   printf("Informe a quantidade de pontos turisticos: ");
   scanf("%d", &pontosTuristicos[0]);
 
+  densidadePopulacional[0] = (float)(populacao[0]/area[0]); 
+  pibPerCarpita[0] = (float)(pib[0]/populacao[0]);
+  superPoder[0] = (float)(populacao[0]+area[0]+pib[0]+pontosTuristicos[0]+pibPerCarpita[0])-densidadePopulacional[0];
+
   //Carta 2
   printf("Carta 2:\n");
   
@@ -62,11 +69,43 @@ int main() {
   printf("Informe a quantidade de pontos turisticos: ");
   scanf("%d", &pontosTuristicos[1]);
 
+  densidadePopulacional[1] = (float)(populacao[1]/area[1]); 
+  pibPerCarpita[1] = (float)(pib[1]/populacao[1]);
+  superPoder[1] = (float)(populacao[1]+area[1]+pib[1]+pontosTuristicos[1]+pibPerCarpita[1])-densidadePopulacional[1];
+
+
   // Área para exibição dos dados da cidade
-  printf("Carta 1:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[0], codigoCarta[0], nomeCidade[0], populacao[0], area[0], pib[0], pontosTuristicos[0], (float)(populacao[0]/area[0]), (float)(pib[0]/populacao[0]));
+  printf("Carta 1:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[0], codigoCarta[0], nomeCidade[0], populacao[0], area[0], pib[0], pontosTuristicos[0], densidadePopulacional[0], pibPerCarpita[0]);
   printf("\n\n");
-  printf("Carta 2:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[1], codigoCarta[1], nomeCidade[1], populacao[1], area[1], pib[1], pontosTuristicos[1], (float)(populacao[0]/area[0]), (float)(pib[0]/populacao[0]));
+  printf("Carta 2:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[1], codigoCarta[1], nomeCidade[1], populacao[1], area[1], pib[1], pontosTuristicos[1], densidadePopulacional[1], pibPerCarpita[1]);
+  printf("\n\n");
+  printf("Comparações entre as cartas\n");
+  printf("População: Carta %d venceu\n",(populacao[0] > populacao[1])? 1 : 2);
+  printf("Área: Carta %d venceu\n",(area[0] > area[1])? 1 : 2);
+  printf("PIB: Carta %d venceu\n",(pib[0] > pib[1])? 1 : 2);
+  printf("Pontos Turísticos: Carta %d venceu\n",(pontosTuristicos[0] > pontosTuristicos[1])? 1 : 2);
+  printf("Densidade Populacional: Carta %d venceu\n",(densidadePopulacional[0] < densidadePopulacional[1])? 1 : 2);
+  printf("PIB per Capita: Carta %d venceu\n",(pibPerCarpita[0] > pibPerCarpita[1])? 1 : 2);
+  printf("Super Poder: Carta %d venceu\n",(superPoder[0] > superPoder[1])? 1 : 2);
+  /*
+    Comparação de Cartas:
+      (O maior vence)
+      População: Carta 1 venceu (1)
+      Área: Carta 1 venceu (1)
+      PIB: Carta 1 venceu (1)
+      Pontos Turísticos: Carta 1 venceu (1)
+      PIB per Capita: Carta 1 venceu (1)
+      Super Poder: Carta 1 venceu (1)
 
+      Densidade Populacional: Carta 2 venceu (0) (A menor dencidade vence)
 
+      int populacao[2];
+      float area[2];
+      float pib[2];
+      int pontosTuristicos[2];
+      float densidadePopulacional[2];
+      float pibPerCarpita[2];
+      float superPoder[2];
+  */
   return 0;
 } 
