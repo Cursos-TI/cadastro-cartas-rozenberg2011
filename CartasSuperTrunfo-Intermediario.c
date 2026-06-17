5518e3f145161944405b6d5c9fb910128796994d
ethimobrasil <ethimnobrasil@gmail.com>
2026-06-17 13:43:26 -0300

    ajustes no exercicio intermediário

--- CartasSuperTrunfo-Intermediario.c
+++ CartasSuperTrunfo-Intermediario.c
@@ -28,7 +28,7 @@ int main() {
   float pib[2];
   int pontosTuristicos[2];
   float densidadePopulacional[2];
-  float pibPerCarpita[2];
+  float pibPerCapita[2];
   float superPoder[2];
 
   int resultadoDigitado, escolhaJogador = -1;
@@ -71,8 +71,8 @@ int main() {
   pontosTuristicos[0] = rand() % 100;
 
   densidadePopulacional[0] = (float)(populacao[0]/area[0]); 
-  pibPerCarpita[0] = (float)(pib[0]/populacao[0]);
-  superPoder[0] = (float)(populacao[0]+area[0]+pib[0]+pontosTuristicos[0]+pibPerCarpita[0])-densidadePopulacional[0];
+  pibPerCapita[0] = (float)(pib[0]/populacao[0]);
+  superPoder[0] = (float)(populacao[0]+area[0]+pib[0]+pontosTuristicos[0]+pibPerCapita[0])-densidadePopulacional[0];
 
   //Carta 2
   /*
@@ -109,8 +109,8 @@ int main() {
   pontosTuristicos[1] = rand() % 100;
 
   densidadePopulacional[1] = (float)(populacao[1]/area[1]); 
-  pibPerCarpita[1] = (float)(pib[1]/populacao[1]);
-  superPoder[1] = (float)(populacao[1]+area[1]+pib[1]+pontosTuristicos[1]+pibPerCarpita[1])-densidadePopulacional[1];
+  pibPerCapita[1] = (float)(pib[1]/populacao[1]);
+  superPoder[1] = (float)(populacao[1]+area[1]+pib[1]+pontosTuristicos[1]+pibPerCapita[1])-densidadePopulacional[1];
 
   //Menu:
 do{
@@ -131,13 +131,13 @@ do{
 
 } while (escolhaJogador <= 0 || escolhaJogador >= 8);
 
-  printf("Sucesso! O numero digitado foi: %d\n", escolhaJogador);  
+  printf("Sucesso! O numero digitado foi: %d.\n", escolhaJogador);  
   printf("\n");
 
   // Área para exibição dos dados da cidade
-  printf("Carta 1:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[0], codigoCarta[0], nomeCidade[0], populacao[0], area[0], pib[0], pontosTuristicos[0], densidadePopulacional[0], pibPerCarpita[0]);
+  printf("Carta 1:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de Reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f Reais\n", estado[0], codigoCarta[0], nomeCidade[0], populacao[0], area[0], pib[0], pontosTuristicos[0], densidadePopulacional[0], pibPerCapita[0]);
   printf("\n\n");
-  printf("Carta 2:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f reais\n", estado[1], codigoCarta[1], nomeCidade[1], populacao[1], area[1], pib[1], pontosTuristicos[1], densidadePopulacional[1], pibPerCarpita[1]);
+  printf("Carta 2:\n Estado: %c\n Código da Carta: %s\n Nome da Cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de Reais\n Quantidades de pontos turísticos: %d\n Densidade Populacional: %.2f hab/km²\n PIB per Capita: %.2f Reais\n", estado[1], codigoCarta[1], nomeCidade[1], populacao[1], area[1], pib[1], pontosTuristicos[1], densidadePopulacional[1], pibPerCapita[1]);
   printf("\n\n");
 
 
@@ -147,37 +147,58 @@ do{
   printf("PIB: Carta %d vence\n",(pib[0] > pib[1])? 1 : 2);
   printf("Pontos Turísticos: Carta %d vence\n",(pontosTuristicos[0] > pontosTuristicos[1])? 1 : 2);
   printf("Densidade Populacional: Carta %d vence\n",(densidadePopulacional[0] < densidadePopulacional[1])? 1 : 2);
-  printf("PIB per Capita: Carta %d vence\n",(pibPerCarpita[0] > pibPerCarpita[1])? 1 : 2);
+  printf("PIB per Capita: Carta %d vence\n",(pibPerCapita[0] > pibPerCapita[1])? 1 : 2);
   printf("Super Poder: Carta %d vence\n",(superPoder[0] > superPoder[1])? 1 : 2);
   printf("\n\nRESULTADO PELA ESCOLHA:\n");
 
   switch(escolhaJogador){
     case 1:
-      printf("Escolha:\n1 - População: Carta %d venceu\n",(populacao[0] > populacao[1])? 1 : 2);
+      printf("Escolha 1 - População:\n");
+      printf("Carta 1 - %s (%c): %d; e\n", nomeCidade[0], estado[0], populacao[0]);
+      printf("Carta 2 - %s (%c): %d.\n", nomeCidade[1], estado[1], populacao[1]);
+      printf("Carta %d venceu.\n",(populacao[0] > populacao[1])? 1 : 2);
       break;
 
     case 2:
-      printf("Escolha:\n2 - Área: Carta %d venceu\n",(area[0] > area[1])? 1 : 2);
+      printf("Escolha 2 - Área: \n");
+      printf("Carta 1 - %s (%c): %.2f km²; e\n", nomeCidade[0], estado[0], area[0]);
+      printf("Carta 2 - %s (%c): %.2f km².\n", nomeCidade[1], estado[1], area[1]);
+      printf("Carta %d venceu.\n",(area[0] > area[1])? 1 : 2);
       break;
 
     case 3:
-      printf("Escolha:\n3 - PIB: Carta %d venceu\n",(pib[0] > pib[1])? 1 : 2);
+      printf("Escolha 3 - PIB: \n");
+      printf("Carta 1 - %s (%c): %.2f bilhões de Reais; e\n", nomeCidade[0], estado[0], pib[0]);
+      printf("Carta 2 - %s (%c): %.2f bilhões de Reais.\n", nomeCidade[1], estado[1], pib[1]);
+      printf("Carta %d venceu.\n",(pib[0] > pib[1])? 1 : 2);
       break;
 
     case 4:
-      printf("Escolha:\n4 - Pontos Turísticos: Carta %d venceu\n",(pontosTuristicos[0] > pontosTuristicos[1])? 1 : 2);
+      printf("Escolha 4 - Pontos Turísticos:\n");
+      printf("Carta 1 - %s (%c): %d; e\n", nomeCidade[0], estado[0], pontosTuristicos[0]);
+      printf("Carta 2 - %s (%c): %d.\n", nomeCidade[1], estado[1], pontosTuristicos[1]);
+      printf("Carta %d venceu.\n",(pontosTuristicos[0] > pontosTuristicos[1])? 1 : 2);
       break;
 
     case 5:
-      printf("Escolha:\n5 - Densidade Populacional: Carta %d venceu\n",(densidadePopulacional[0] < densidadePopulacional[1])? 1 : 2);
+      printf("Escolha 5 - Densidade Populacional:\n");
+      printf("Carta 1 - %s (%c): %.2f hab/km²; e\n", nomeCidade[0], estado[0], densidadePopulacional[0]);
+      printf("Carta 2 - %s (%c): %.2f hab/km².\n", nomeCidade[1], estado[1], densidadePopulacional[1]);
+      printf("Carta %d venceu.\n",(densidadePopulacional[0] < densidadePopulacional[1])? 1 : 2);
       break;
 
     case 6:
-      printf("Escolha:\n6 - PIB per Capita: Carta %d venceu\n",(pibPerCarpita[0] > pibPerCarpita[1])? 1 : 2);
+      printf("Escolha 6 - PIB per Capita:\n");
+      printf("Carta 1 - %s (%c): %.2f Reais; e\n", nomeCidade[0], estado[0], pibPerCapita[0]);
+      printf("Carta 2 - %s (%c): %.2f Reais.\n", nomeCidade[1], estado[1], pibPerCapita[1]);
+      printf("Carta %d venceu.\n",(pibPerCapita[0] > pibPerCapita[1])? 1 : 2);
       break;
 
     case 7:
-      printf("Escolha:\n7 - Super Poder: Carta %d venceu\n",(superPoder[0] > superPoder[1])? 1 : 2);
+      printf("Escolha 7 - Super Poder:\n");
+      printf("Carta 1 - %s (%c): %.2f; e\n", nomeCidade[0], estado[0], superPoder[0]);
+      printf("Carta 2 - %s (%c): %.2f.\n", nomeCidade[1], estado[1], superPoder[1]);
+      printf("Carta %d venceu.\n",(superPoder[0] > superPoder[1])? 1 : 2);
       break;
   }
 
@@ -222,7 +243,7 @@ do{
       float pib[2];
       int pontosTuristicos[2];
       float densidadePopulacional[2];
-      float pibPerCarpita[2];
+      float pibPerCapita[2];
       float superPoder[2];
   
 
